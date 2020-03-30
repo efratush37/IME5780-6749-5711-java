1b20c00278e812ad2c1a3d7856f55d8b733ab298
Efrat <Efrat@Efrat>
2020-03-31 01:53:09 +0300

    Add test and Junit

---
+++ src/unittests/geometries/PlaneTests.java
@@ -0,0 +1,12 @@
+package unittests.geometries;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlaneTests {
+
+    @Test
+    public void getNormal() {
+    }
+}
---
+++ src/unittests/primitives/VectorTests.java
@@ -0,0 +1,44 @@
+package unittests.primitives;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VectorTests {
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void scale() {
+    }
+
+    @Test
+    public void lengthSquared() {
+    }
+
+    @Test
+    public void length() {
+    }
+
+    @Test
+    public void dotProduct() {
+    }
+
+    @Test
+    public void crossProduct() {
+    }
+
+    @Test
+    public void normalize() {
+    }
+
+    @Test
+    public void normalized() {
+    }
+}
